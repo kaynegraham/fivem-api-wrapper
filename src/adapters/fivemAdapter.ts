c8c96e847942b71698a45d1d595027a86c754ca6
Kayne Graham <kaynegrahamdev@icloud.com>
2026-02-07 23:25:22 +0800

    add: streamlined fivem adapter for url checking and individual files

--- src/adapters/fivemAdapter.ts
+++ src/adapters/fivemAdapter.ts
@@ -1,23 +1,13 @@
 import { config } from "../config/env";
 import { safeFetchJson } from "../utils/safeFetch";
+let endpointURL: URL;
 
-export async function fetchFiveMData(baseURL: URL) {
-  try {
-    baseURL = new URL(config.FIVEM_BASE_URL);
-  } catch {
-    throw new Error(
-      `FiveM_Base_URL error: Ensure your .env document matches the format of the example provided in the root folder.`,
-    );
-  }
+export const getDynamic = async (baseURL: string, endPoint: string) => {
+  endpointURL = new URL(endPoint, baseURL);
+  return safeFetchJson(endpointURL, config.FIVEM_TIMEOUT);
+};
 
-  const dynamicURL = new URL("/dynamic.json", baseURL);
-  const playersURL = new URL("/players.json", baseURL);
-
-  const getDynamic = async (dynamicURL: URL) =>
-    safeFetchJson(dynamicURL, config.FIVEM_TIMEOUT);
-
-  const getPlayers = async (playersURL: URL) =>
-    safeFetchJson(playersURL, config.FIVEM_TIMEOUT);
-
-  return { dynamic: getDynamic(dynamicURL), players: getPlayers(playersURL) };
-}
+export const getPlayer = async (baseURL: string, endPoint: string) => {
+  endpointURL = new URL(endPoint, baseURL);
+  return safeFetchJson(endpointURL, config.FIVEM_TIMEOUT);
+};
--- src/utils/parseFivem.ts
+++ src/utils/parseFivem.ts
@@ -1,5 +1,8 @@
+import { getDynamic, getPlayer } from "../adapters/fivemAdapter";
 import { config } from "../config/env";
 
 export async function parseFivem() {
-  console.log(config.FIVEM_BASE_URL);
+  const serverData = await getDynamic(config.FIVEM_BASE_URL, "dynamic.json");
+  const playerData = await getPlayer(config.FIVEM_BASE_URL, "players.json");
+  return { serverData: serverData, playerData: playerData };
 }
