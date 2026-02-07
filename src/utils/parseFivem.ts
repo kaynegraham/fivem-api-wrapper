da66d88976bd7d9605399bd14c56bd74a8c2d0f0
Kayne Graham <kaynegrahamdev@icloud.com>
2026-02-08 00:37:28 +0800

    chore: update parse fivem

--- src/utils/parseFivem.ts
+++ src/utils/parseFivem.ts
@@ -2,13 +2,13 @@ import { ServerInformation, Player } from "../types/fivem";
 
 export function parseFivem(rawDynamic: any, rawPlayers: any) {
   const serverInfo: ServerInformation = {
-    name: rawDynamic.data.server || "Unknown Server Name",
-    map: rawDynamic.data.map || "Unknown Map",
-    currentPlayers: rawDynamic.data.clients || 0,
-    maxPlayers: rawDynamic.data.sv_maxclients || 0,
+    name: rawDynamic.data?.hostname || "Unknown Server Name",
+    map: rawDynamic.data?.mapname || "Unknown Map",
+    currentPlayers: rawDynamic.data?.clients || 0,
+    maxPlayers: rawDynamic.data?.sv_maxclients || 0,
   };
 
-  const playerInfo: Player[] = Object.values(rawPlayers).map((item: any) => {
+  const playerInfo: Player[] = rawPlayers.data?.map((item: any) => {
     return {
       name: item.name || "Unknown Player Name",
       id: item.id || "Unknown Player ID",
